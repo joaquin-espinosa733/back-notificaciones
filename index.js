f4e110b84b1c84a312929d222101595b4a1cf56d
joaquin-espinosa733 <juaco.enero9@gmail.com>
2024-03-12 21:26:42 -0300

    corsOptiions

--- index.js
+++ index.js
@@ -11,10 +11,21 @@ const corsOptions = {
     credentials: true,
 };
 
+
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(cors(corsOptions));
-
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin, same-origin-allow-popups');
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 
 app.get("/", (req, res) => {
